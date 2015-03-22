f9fdd58b27c139fb75a9c5e8119e97a5cc8c0a27
Adrienne <adrie.k@gmail.com>
2015-03-21 17:05:43 -0700

    added pages

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var express = require('express');
-
 var app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-  res.render('index');
+  res.render('home');
+});
+
+app.get('/map', function(req, res){
+  res.render('map');
 });
 
 app.listen(process.env.PORT || 3000, function(){
