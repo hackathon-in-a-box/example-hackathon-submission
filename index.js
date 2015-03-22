09fb4bdecd988cba6b44bc4cdcb773ad8e98558c
Adrienne <adrie.k@gmail.com>
2015-03-22 14:19:29 -0700

    hard coded pages

--- index.js
+++ index.js
@@ -8,9 +8,30 @@ app.get('/', function(req, res){
   res.render('home');
 });
 
+app.get('/about', function(req, res){
+  res.render('about');
+});
+
+app.get('/map/car', function(req, res){
+  res.render('car');
+});
+
+app.get('/map/bike', function(req, res){
+  res.render('bike');
+});
+
+app.get('/map/bus', function(req, res){
+  res.render('bus');
+});
+
+app.get('/map/walk', function(req, res){
+  res.render('walk');
+});
+
 app.get('/map', function(req, res){
   res.render('map');
 });
+
 
 app.listen(process.env.PORT || 3000, function(){
     console.log('DEATH RACE 3000!');
